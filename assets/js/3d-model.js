1084b48404f2858ea42a71fec1c17593f881a6fd
seth-mc <64566764+seth-mc@users.noreply.github.com>
2023-03-19 13:36:12 -0600

    🧞‍adding model to scene

--- assets/js/3d-model.js
+++ assets/js/3d-model.js
@@ -1,7 +1,10 @@
 import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
 let camera, scene, renderer, model, controls;
+
+const loader = new GLTFLoader();
+
 
 init();
 
@@ -17,9 +20,36 @@ function init() {
   document.body.appendChild(renderer.domElement);
 
   // Create the model
+  // Load a glTF resource
+  loader.load(
+    // resource URL
+    './assets/stikpack.glb',
+    // called when the resource is loaded
+    function ( gltf ) {
+    // get the loaded model
+    model = gltf.scene;
+
+    model.position.set(0, 0, 0);
+    model.rotation.set(0, 0, 0);
+    model.scale.set(1, 1, 1);
+
+    // add the model to the scene
+    scene.add( model );
+    },
+    // called while loading is progressing
+    function ( xhr ) {
+    console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+    },
+    // called when loading has errors
+    function ( error ) {
+    console.error( error );
+    }
+);
+
+
   const geometry = new THREE.BoxGeometry();
   const material = new THREE.MeshStandardMaterial({ color: 0x00ff00 });
-  model = new THREE.Mesh(geometry, material);
+  model  = new THREE.Mesh(geometry, material);
   scene.add(model);
 
   // Add lights
@@ -29,11 +59,6 @@ function init() {
   const pointLight = new THREE.PointLight(0xffffff, 1);
   pointLight.position.set(10, 10, 10);
   scene.add(pointLight);
-
-  // Add orbit controls
-  controls = new OrbitControls(camera, renderer.domElement);
-  controls.enableDamping = true;
-  controls.dampingFactor = 0.05;
 
   // Add event listeners
   window.addEventListener('resize', onWindowResize, false);
@@ -48,11 +73,6 @@ function animate() {
   // Rotate the model
   model.rotation.y += 0.01;
 
-  // Ascend the model
-  model.position.y += 0.005;
-
-  // Update the orbit controls
-  controls.update();
 
   // Render the scene
   renderer.render(scene, camera);
